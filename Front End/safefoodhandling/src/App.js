9acd69dcb23231e069b53d0b746955c826670222
Naveen Reddy <nrkb1998@gmail.com>
2023-01-27 20:23:38 -0500

    Added index, nav, footer pages.

--- Front End/safefoodhandling/src/App.js
+++ Front End/safefoodhandling/src/App.js
@@ -1,34 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
-
+import NavBar from './Components/Navbar';
 function App() {
   return (
     <div className="App">
+      <NavBar />
+      <h1> SAFE FOOD HANDLING WEBSITE COMMING SOON...</h1>
+
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <h1> SAFE FOOD HANDLING WEBSITE COMMING SOON...</h1>
 
 
-        <h2> this is Akhil K</h2>
-
-        <h2>This Prince</h2>
-
-        <h2>This is Sonika</h2>
-
-      <h1>write a statement</h1>
-
-
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
